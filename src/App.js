1a7b3394703d71775cb1bfe7077d71bcab1b055a
Agustin Retamar <agustinretamar97@gmail.com>
2023-09-07 11:00:08 -0300

    second commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          ARTraining ecom
         </p>
         <a
           className="App-link"
